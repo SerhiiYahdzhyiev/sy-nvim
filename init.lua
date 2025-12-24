0f846915290403dba0e3b370cad59cafaa955a16
Serhii Yahdzhyiev <yahdzhyiev@perfacct.eu>
2025-12-24 12:03:49 +0100

    Add live grep keybinding to telescope config.

--- init.lua
+++ init.lua
@@ -222,6 +222,7 @@ require("lazy").setup({
 			-- See `:help telescope.builtin`
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live [G]rep" })
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[S]earch [F]iles" })
 			vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
 			vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
